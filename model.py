124b529fcaac389b330440d9a369daa7b9ac511c
Vaibhav <mysteryvaibhav@gmail.com>
2018-02-21 22:45:13 -0500

    Made some chnages

--- model.py
+++ model.py
@@ -5,25 +5,24 @@ import torch.nn.functional as F
 class Generator(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Generator, self).__init__()
-        self.map1 = nn.Linear(input_size, hidden_size)
-        self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.map3 = nn.Linear(hidden_size, output_size)
+        self.map1 = nn.Linear(input_size, output_size, bias=False)
 
     def forward(self, x):
-        x = F.relu(self.map1(x))
-        x = F.tanh(self.map2(x))
-        return self.map3(x)
+        return self.map1(x)
 
 
 class Discriminator(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Discriminator, self).__init__()
         self.map1 = nn.Linear(input_size, hidden_size)
-        self.drop = nn.Dropout(0.1)
+        self.drop1 = nn.Dropout(0.1)
+        self.activation1 = nn.LeakyReLU(0.2)
         self.map2 = nn.Linear(hidden_size, hidden_size)
+        self.drop2 = nn.Dropout(0.1)
+        self.activation2 = nn.LeakyReLU(0.2)
         self.map3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.map1(self.drop(x)))
-        x = F.relu(self.map2(x))
-        return F.sigmoid(self.map3(x))
+        x = self.activation1(self.map1(self.drop1(x)))
+        x = self.activation2(self.map2(self.drop2(x)))
+        return F.sigmoid(self.map3(x)).view(-1)
--- trainer.py
+++ trainer.py
@@ -19,40 +19,45 @@ def to_variable(tensor):
     return torch.autograd.Variable(tensor)
 
 
-class CustomDataSet(torch.utils.data.TensorDataset):
-    def __init__(self, data):
-        self.data = data
-        self.num_of_samples = len(self.data)
+def get_batch_data(en, it, g, detach=False):
+    en_len = en.shape[0]
+    it_len = it.shape[0]
+    random_en_indices = np.random.permutation(en_len)
+    random_it_indices = np.random.permutation(it_len)
+    en_batch = en[random_en_indices[:mini_batch_size]]
+    it_batch = it[random_it_indices[:mini_batch_size]]
+    fake = g(to_variable(to_tensor(en_batch)))
+    if detach:
+        fake.detach()
+    real = to_variable(to_tensor(it_batch))
+    input = torch.cat([fake, real], 0)
+    output = to_variable(torch.FloatTensor(2 * mini_batch_size).zero_())
+    output[:mini_batch_size] = 1 - smoothing
+    output[mini_batch_size:] = smoothing
+    return input, output
 
-    def __len__(self):
-        return self.num_of_samples
-
-    def __getitem__(self, idx):
-        return to_tensor(self.data[idx])
 
 
 def train():
     # Load data
     en, it = get_embeddings()   # Vocab x Embedding_dimension
 
-    # Create data-loaders
-    en_data_loader = torch.utils.data.DataLoader(CustomDataSet(en), batch_size=mini_batch_size, shuffle=True)
-    it_data_loader = torch.utils.data.DataLoader(CustomDataSet(it), batch_size=mini_batch_size, shuffle=True)
-
     # Create models
     g = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)
     d = Discriminator(input_size=d_input_size, hidden_size=d_hidden_size, output_size=d_output_size)
 
     # Define loss function and optimizers
-    loss_fn = torch.nn.BCELoss()
-    d_optimizer = optim.Adam(d.parameters(), lr=d_learning_rate, betas=optim_betas)
-    g_optimizer = optim.Adam(g.parameters(), lr=g_learning_rate, betas=optim_betas)
+    d_loss_fn = torch.nn.BCELoss()
+    d_optimizer = optim.SGD(d.parameters(), lr=d_learning_rate, weight_decay=0.95)
+    g_optimizer = optim.SGD(g.parameters(), lr=g_learning_rate, weight_decay=0.95)
+    g_loss_fn = torch.nn.BCELoss()
 
     if torch.cuda.is_available():
         # Move the network and the optimizer to the GPU
         g = g.cuda()
         d = d.cuda()
-        loss_fn = loss_fn.cuda()
+        d_loss_fn = d_loss_fn.cuda()
+        g_loss_fn = g_loss_fn.cuda()
 
     d_acc = []
     for epoch in range(num_epochs):
@@ -61,68 +66,39 @@ def train():
         hit = 0
         total = 0
         start_time = timer()
-        en_iter = iter(en_data_loader)
-        mini_batch = 1
-        for d_real_data in it_data_loader:
-            # Inspired from https://github.com/devnag/pytorch-generative-adversarial-networks/blob/master/gan_pytorch.py
+        for mini_batch in range(0, len(en) // mini_batch_size):
             for d_index in range(d_steps):
-                # 1. Train D on real+fake
-                d.zero_grad()  # Reset the gradients
-
-                #  1A: Train D on real
-                noise = torch.normal(torch.ones(mini_batch_size, d_input_size) * 5,
-                                     torch.ones(mini_batch_size, d_input_size) * 2)
-                d_real_data = to_variable(torch.mul(d_real_data, noise))  # Could add some noise to the real data later
-
-                d_real_decision = d(d_real_data)
-                real_discriminator_decision = d_real_decision.data.cpu().numpy()
-                hit += np.sum(real_discriminator_decision < 0.5)
-                d_real_error = loss_fn(d_real_decision, to_variable(torch.zeros(mini_batch_size, 1)))  # ones = true
-                d_real_error.backward()  # compute/store gradients, but don't change params
-                d_losses.append(d_real_error.data.cpu().numpy())
-
-                #  1B: Train D on fake
-                d_gen_input = to_variable(next(en_iter))
-                d_fake_data = g(d_gen_input).detach()  # detach to avoid training G on these labels
-                d_fake_decision = d(d_fake_data)  # Add noise later
-                fake_discriminator_decision = d_fake_decision.data.cpu().numpy()
-                hit += np.sum(fake_discriminator_decision >= 0.5)
-                d_fake_error = loss_fn(d_fake_decision, to_variable(torch.ones(mini_batch_size, 1)))  # zeros = fake
-                d_fake_error.backward()
-                d_losses.append(d_fake_error.data.cpu().numpy())
+                d_optimizer.zero_grad()  # Reset the gradients
+                # noise = torch.normal(torch.ones(mini_batch_size, d_input_size) * 5,
+                #                      torch.ones(mini_batch_size, d_input_size) * 2)
+                input, output = get_batch_data(en, it, g, detach=True)
+                pred = d(input)
+                d_loss = d_loss_fn(pred, output)
+                d_loss.backward()  # compute/store gradients, but don't change params
+                d_losses.append(d_loss.data.cpu().numpy())
+                discriminator_decision = pred.data.cpu().numpy()
+                hit += np.sum(discriminator_decision < 0.5)
                 d_optimizer.step()  # Only optimizes D's parameters; changes based on stored gradients from backward()
                 sys.stdout.write("[%d/%d] :: Discriminator Loss: %f \r" % (
                     mini_batch, len(en) // mini_batch_size, np.asscalar(np.mean(d_losses))))
                 sys.stdout.flush()
-                mini_batch += 1
-                total += 2*mini_batch_size
+            total += 2*mini_batch_size*d_steps
 
-        mini_batch = 1
-        it_iter = iter(it_data_loader)
-        for gen_input in en_data_loader:
             for g_index in range(g_steps):
                 # 2. Train G on D's response (but DO NOT train D on these labels)
-                g.zero_grad()
+                g_optimizer.zero_grad()
 
-                gen_input = to_variable(gen_input)
-                g_fake_data = g(gen_input)
-                g_fake_decision = d(g_fake_data)  # Add noise later
-                g_error = loss_fn(g_fake_decision,
-                                  to_variable(torch.zeros(mini_batch_size, 1)))  # we want to fool, so pretend it's all genuine
-                g_losses.append(g_error.data.cpu().numpy())
-                g_error.backward()
-
-                g_input_real = to_variable(next(it_iter))
-                real_decision = d(g_input_real)
-                g_real_error = loss_fn(real_decision, to_variable(torch.ones(mini_batch_size, 1)))
-                g_real_error.backward()
-                g_losses.append(g_real_error.data.cpu().numpy())
+                input, output = get_batch_data(en, it, g, detach=False)
+                pred = d(input)
+                g_loss = d_loss_fn(pred, 1 - output)
+                g_loss.backward()
+                g_losses.append(g_loss.data.cpu().numpy())
                 g_optimizer.step()  # Only optimizes G's parameters
 
                 sys.stdout.write("[%d/%d] :: Generator Loss: %f \r" % (
                     mini_batch, len(en) // mini_batch_size, np.asscalar(np.mean(g_losses))))
                 sys.stdout.flush()
-                mini_batch += 1
+
         d_acc.append(hit/total)
         print("Epoch {} : Discriminator Loss: {:.5f}, Discriminator Accuracy: {:.5f}, Generator Loss: {:.5f}, Time elapsed {:.2f} mins".
               format(epoch, np.asscalar(np.mean(d_losses)), hit/total, np.asscalar(np.mean(g_losses)),
