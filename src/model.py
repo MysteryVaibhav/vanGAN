6d3f18af37db7ccec4b84cf08f77e2a5ef3a329c
vaibhav <vvaibhav@andrew.cmu.edu>
2018-03-21 01:09:31 -0400

    dropout only at input layer, identity initialization for generator weights

--- src/model.py
+++ src/model.py
@@ -4,10 +4,11 @@ import torch.nn.functional as F
 
 
 class Generator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, output_size):
         super(Generator, self).__init__()
         self.map1 = nn.Linear(input_size, output_size, bias=False)
-        nn.init.orthogonal(self.map1.weight)
+        #nn.init.orthogonal(self.map1.weight)
+        nn.init.eye(self.map1.weight)   # As per the fb implementation initialization
 
     def forward(self, x):
         return self.map1(x)
@@ -20,11 +21,11 @@ class Discriminator(nn.Module):
         self.drop1 = nn.Dropout(0.1)
         self.activation1 = nn.LeakyReLU(0.2)
         self.map2 = nn.Linear(hidden_size, hidden_size)
-        self.drop2 = nn.Dropout(0.1)
+        self.drop2 = nn.Dropout(0)    # As per the fb implementation
         self.activation2 = nn.LeakyReLU(0.2)
         self.map3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = self.drop1(self.activation1(self.map1(x)))
+        x = self.activation1(self.map1(self.drop1(x))) # Input dropout
         x = self.drop2(self.activation2(self.map2(x)))
         return F.sigmoid(self.map3(x)).view(-1)
