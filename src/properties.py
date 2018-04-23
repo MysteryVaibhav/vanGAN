db23fd67af665468225ea64e90e71ee7dc6a191e
MysteryVaibhav <mysteryvaibhav@gmail.com>
2018-04-23 02:07:22 +0000

    Changing to default values

--- src/properties.py
+++ src/properties.py
@@ -39,11 +39,11 @@ mini_batch_size = 32
 
 d_learning_rate = 0.2
 g_learning_rate = 0.2
-num_epochs = 15
+num_epochs = 7
 d_steps = 5  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
 g_steps = 1
 smoothing = 0.1   # As per what is mentioned in the paper
-beta = 0.0001
+beta = 0.001
 clip_value = 0
 
 # Training
